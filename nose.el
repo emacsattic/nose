3dec4fce4800ee0eb1c4e0940bd2da6f5ab10dff
David Beswick <dlbeswick@gmail.com>
2014-04-11 13:24:58 +1000

    Added nosetests-again keybind to documentation.

--- nose.el
+++ nose.el
@@ -47,6 +47,7 @@
 ;;             (local-set-key "\C-ca" 'nosetests-all)
 ;;             (local-set-key "\C-cm" 'nosetests-module)
 ;;             (local-set-key "\C-c." 'nosetests-one)
+;;             (local-set-key "\C-c\C-c" 'nosetests-again)
 ;;             (local-set-key "\C-cpa" 'nosetests-pdb-all)
 ;;             (local-set-key "\C-cpm" 'nosetests-pdb-module)
 ;;             (local-set-key "\C-cp." 'nosetests-pdb-one)))
